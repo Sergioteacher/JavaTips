6a9a4513d0cd29affbc71340229543816d000c88
Sergio Teacher <Sergio Teacher@MiniWin>
2023-11-10 12:45:25 +0100

    Añado etiquetas de relleno

--- src/javaTips/JavaTips.java
+++ src/javaTips/JavaTips.java
@@ -7,6 +7,8 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
 import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class JavaTips {
 
@@ -48,9 +50,26 @@ public class JavaTips {
 		frmEjemploDelUso.getContentPane().add(panel_principal, BorderLayout.CENTER);
 		panel_principal.setLayout(new BorderLayout(0, 0));
 		
+		JLabel lblUp = new JLabel("Un texto en la zona superior");
+		lblUp.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblUp.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_principal.add(lblUp, BorderLayout.NORTH);
+		
 		JButton btnBotonCentral = new JButton("pulsame ...");
 		btnBotonCentral.setFont(new Font("Trebuchet MS", Font.ITALIC, 18));
 		panel_principal.add(btnBotonCentral, BorderLayout.CENTER);
+		
+		JLabel lblDown = new JLabel("Un texto inferior");
+		lblDown.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDown.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panel_principal.add(lblDown, BorderLayout.SOUTH);
+		
+		JLabel lblLeft = new JLabel("Izquierda");
+		lblLeft.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		panel_principal.add(lblLeft, BorderLayout.WEST);
+		
+		JLabel lblRight = new JLabel("Derecha");
+		panel_principal.add(lblRight, BorderLayout.EAST);
 	}
 
 }
